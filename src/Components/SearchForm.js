d4a9fc944857a7226b53b54162f4ea8a76b9d645
PK <pradeepdhital124@gmail.com>
2022-06-23 16:05:46 +1000

    button corrected

--- src/Components/SearchForm.js
+++ src/Components/SearchForm.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
-import Button from "react-bootstrap/Btton";
+import Button from "react-bootstrap/Button";
 
 export const SearchForm = ({ handleOnSubmit }) => {
   const [str, setStr] = useState("");
