eedcc314252bef0130e621f11aefc325f89fadcd
PK <pradeepdhital124@gmail.com>
2023-03-04 09:56:04 +1100

    http changed to https for https request

--- src/Helper/axiosHelper.js
+++ src/Helper/axiosHelper.js
@@ -1,11 +1,10 @@
 import axios from "axios";
-const apiEndPoint = "http://www.omdbapi.com/?apikey=461da771&";
+const apiEndPoint = "https://www.omdbapi.com/?apikey=461da771&";
 
 export const fetchMovieInfo = async (title) => {
   try {
     const url = apiEndPoint + "t=" + title;
     const { data } = await axios.get(url);
-
     return data;
   } catch (error) {
     return { status: "error", mesage: error.message };
